add031f1294aa70b32da4fcd10719525766ac4b6
SmitasCodes <airidassmith30@gmail.com>
2023-08-17 16:20:42 +0300

    made some visible changes to menu

--- modules/continentFilter.js
+++ modules/continentFilter.js
@@ -4,25 +4,32 @@ import countriesOutput from "./countriesOutput.js";
 // Event Listeners for navbar
 document.querySelectorAll(".menu-item").forEach((item) => {
   item.addEventListener("click", function (e) {
-    document.querySelectorAll(".menu-item.selected").forEach((selectedItem) => {
-      selectedItem.classList.remove("selected");
-    });
-
-    item.classList.add("selected");
-    document.documentElement.style.setProperty(
-      "--selected-color",
-      item.dataset.selectedColor
-    );
-
     const target = e.target.id;
     filter(target);
   });
 });
 
+// Function for chaning colors in menu once you click on specific continent
+const changingMenuColor = (continent) => {
+  document.querySelectorAll(".menu-item.selected").forEach((selectedItem) => {
+    selectedItem.classList.remove("selected");
+  });
+
+  const aElement = document.querySelector(`#${continent}`);
+
+  aElement.classList.add("selected");
+  document.documentElement.style.setProperty(
+    "--selected-color",
+    aElement.dataset.selectedColor
+  );
+  return;
+};
+
 // Filtering function
 const filter = (continent) => {
   main.innerHTML = "";
   countriesOutput();
+  changingMenuColor(continent);
   continentsColorsSvg(continent);
   document.querySelectorAll(".country").forEach((country) => {
     continent == "all"
